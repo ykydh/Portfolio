3ceacf1edfb62546cd399cf5c344ff55900326a8
Yuatro Kiyota <ykydh@umsystem.edu>
2025-09-19 14:21:19 -0500

    pic

--- projects/Codes/map/main.cpp
+++ projects/Codes/map/main.cpp
@@ -2,29 +2,32 @@
 #include <iostream>
 #include <string>
 
-// Function to build a map of character counts
+// Count character frequencies into map
 void get_char_frequency(MyMap<char, int>& in_tree) {
-    std::string file_input;
-    while(std::getline(std::cin,file_input))
-    {
-      for(int i = 0; i < static_cast<int>(file_input.size());i++)
-      {
-        in_tree[file_input[i]] += 1;
-      }
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        for (char c : line) {
+            in_tree[c] += 1;   // operator[] creates or updates counts
+        }
     }
 }
 
 int main() {
-  MyMap<char, int> map_obj;
-  std::string first_line;
-  std::getline(std::cin,first_line);
-  
-  get_char_frequency(map_obj);
-  for(int i =0;i <static_cast<int>(map_obj.size());i++)
-  {
-    std::cout<<static_cast<char>(map_obj[first_line[i]]);
-  }
-  std::cout<<"\n";
+    MyMap<char, int> map_obj;
+
+    // Read the first line separately (just as your original code did)
+    std::string first_line;
+    std::getline(std::cin, first_line);
+
+    // Process the rest of the input
+    get_char_frequency(map_obj);
+
+    std::cout << "Character frequencies from input:\n";
+    for (char c : first_line) {
+        std::cout << "'" << c << "' : " << map_obj[c] << "\n";
+    }
+
+    std::cout << "\nTotal unique characters: " << map_obj.size() << "\n";
+
     return 0;
 }
-
